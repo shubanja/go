966e145b7ab9fc2555ad34132081e6cb26bb66c4
shubanja <mendoza92@yandex.ru>
2024-06-05 23:35:55 +0300

    task a,b

--- Lec2/main.go
+++ Lec2/main.go
@@ -3,11 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world", ,"Hello another")
-	fmt.Println("second line")
-	fmt.Print("first")
-	fmt.Print("second")
-	fmt.Print("Third")
-	fmt.Printf("\n Hello, my name is %s \n My age is %d \n Hi", "Bob", 42)
+	fmt.Println("A", "Привет! Я новый студент!")
+	fmt.Println("B", "Это моя вторая программа! Я рад, что учусь здесь!!")
+	// fmt.Print("first")
+	// fmt.Print("Third")
+	// fmt.Printf("\n Hello, my name is %s \n My age is %d \n Hi", "Bob", 42)
 	//форматированный вывод
 }
