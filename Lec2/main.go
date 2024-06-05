253cc809ad560500e57a5ec09464035b8af7d60f
shubanja <mendoza92@yandex.ru>
2024-06-05 22:59:03 +0300

    two

---
+++ Lec2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world", ,"Hello another")
+	fmt.Println("second line")
+	fmt.Print("first")
+	fmt.Print("second")
+	fmt.Print("Third")
+	fmt.Printf("\n Hello, my name is %s \n My age is %d \n Hi", "Bob", 42)
+	//форматированный вывод
+}
