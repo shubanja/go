fe0b8a36375d7997f087ec6f1bc7698ec2702699
shubanja <mendoza92@yandex.ru>
2024-06-06 00:24:46 +0300

    lic 3

--- Lec2/main.go
+++ Lec2/main.go
@@ -9,4 +9,30 @@ func main() {
 	// fmt.Print("Third")
 	// fmt.Printf("\n Hello, my name is %s \n My age is %d \n Hi", "Bob", 42)
 	//форматированный вывод
+	////////////////
+	//Декларирование переменной
+	var age int
+	fmt.Println("My age is:", age)
+	age = 32
+	fmt.Println("Age after assignment:", age)
+
+	// Декларирование и инициализация пользовательским значением
+	var height int = 183
+	fmt.Println("My height is:", height)
+
+	// в чем полустрогость
+	var uid = 123123
+	fmt.Println("My passport:", uid)
+	fmt.Printf("%T", uid)
+
+	///
+	var firstVar, secondVar int = 20, 30
+	fmt.Printf("firstVar:%d, secondVar:%d\n", firstVar, secondVar)
+	////
+	var (
+		personName string = "bob"
+		personAge  int    = 19
+		personId   int
+	)
+	fmt.Printf("personName: %s\nAge: %d\nUID: %d\n", personName, personAge, personId)
 }
