826b47a71c42619fafde38a3bba8626bfaec827a
shubanja <mendoza92@yandex.ru>
2024-06-06 14:58:51 +0300

    LeC3

--- Lec3/Lec3_2/main.go
+++ Lec3/Lec3_2/main.go
@@ -18,7 +18,7 @@ func main() {
 	fmt.Printf("my name is: %s\nMy age is: %d\n", name, age)
 	fmt.Printf("name type: %T\nage type: %T\n", name, age)
 	//Для ручного потока ввода Fsacn
-	fmt.Print("Еще раз сколько лет?\n")
+	fmt.Print("Что то пошло не так, еще раз сколько лет??\n")
 	fmt.Fscan(os.Stdin, &age)
-	fmt.Println("New age:", age)
+	fmt.Printf("Все отлично записал Вам: %d лет", age)
 }
