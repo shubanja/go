c9644275d8aa4faed0dab328b5b14446572cbf32
shubanja <mendoza92@yandex.ru>
2024-06-08 21:10:30 +0300

    up

--- Specialist/Lec4/main.go
+++ Specialist/Lec4/main.go
@@ -5,5 +5,17 @@ import "fmt"
 func main() {
 	//Boolean
 	var firsBoolean bool
-	fmt.Print(firsBoolean)
+	fmt.Println(firsBoolean)
+	//Boolean operand
+	aBoolean, bBoolean := true, true
+	fmt.Println("AND:", aBoolean && bBoolean) // логическое умнажение  И - &&
+	fmt.Println("OR:", aBoolean || bBoolean)  // логическое И
+	fmt.Println("NOT:", !aBoolean)            // ! Отрицание
+
+	//Numerics. Integres
+	//int8, int16, int32, int64
+	//uint8, uint16, uint32, uint64
+	var a int = 32
+	b := 92
+	fmt.Println("Value of a:", a, "Value of b:", b, "Sum of a+b:", a+b)
 }
